53d77fdcddc7d24fda6bcecf17a073861cf20f4c
GillesDCI <gilles.vanhaecke2@gmail.com>
2023-10-26 11:49:44 +0200

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+const main = () => {
+    console.log("This is the main logic, make a change from the login branch.")
+    console.log("This is the main logic, adding this from the main branch.")
+}
+
+const login = () => {
+    console.log("This is the login functionality we are adding");
+}
